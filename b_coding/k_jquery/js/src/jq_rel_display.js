eda9499369033aa68717cf6fe971fc667fd5a2e0
xido <erivery2@gmail.com>
2020-05-11 15:04:39 +0900

    아코디언메뉴1

--- b_coding/k_jquery/js/src/jq_rel_display.js
+++ b_coding/k_jquery/js/src/jq_rel_display.js
@@ -4,30 +4,23 @@
 // jquery start =======================
  var st01 = $('.style_01');
  var st01Li = st01.find('li');
-
  var con  = $('.content');
  var conLi = con.find('div');
 
-//  st01Li.eq(0).addClass('action');
+ st01Li.eq(0).addClass('action');
 
- var liBg = st01Li.eq(0).css('backgroundColor');
-	var c01 = conLi.eq(0).css('backgroundColor');
-	st01Li.eq(0).css({'backgroundColor':c01});
-
- st01Li.on('click', function(e){
+ st01Li.children('button').on('click focus', function(e){
 	e.preventDefault();
-	// $(this).addClass('action');
-	// $(this).siblings().removeClass('action');	
-	
-	var i = $(this).index();
-	// conLi.hide();
-	conLi.eq(i).siblings().stop().hide();
-	conLi.eq(i).stop().fadeIn();
 
-	var conCol = conLi.eq(i).css('backgroundColor');
-	$(this).siblings().css({'backgroundColor':liBg});
-	$(this).css({'backgroundColor':conCol});
+	var myThis = $(this).parent('li');
+	var i = myThis.index();
+	var conLiEq = conLi.eq(i);
 
+	myThis.addClass('action');
+	myThis.siblings().removeClass('action');	
+
+	conLiEq.siblings().stop().hide();
+	conLiEq.stop().fadeIn();
  });
 
 
