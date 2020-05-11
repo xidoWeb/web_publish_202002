004c6328b836fd5fdb50b8cdb15a7960275d0413
xido <erivery2@gmail.com>
2020-05-11 15:31:11 +0900

    아코디언메뉴01 -02

--- b_coding/k_jquery/js/src/jq_rel_display.js
+++ b_coding/k_jquery/js/src/jq_rel_display.js
@@ -22,6 +22,25 @@
 	conLiEq.siblings().stop().hide();
 	conLiEq.stop().fadeIn();
  });
+// **********************************************
+var accMenu = $('.accordion_menu');
+var accSt01 = accMenu.children('.acc_style_01');
+var accSt01_dt = accSt01.find('dt');
+var accSt01_dd = accSt01.find('dd');
+
+// console.log( accSt01_dt.length, accSt01_dd.length );
+
+// dt('accSt01_dt')를 클릭시
+accSt01_dt.on('click', function(){
+	// 이미 열려있는 dd는 접히고,(단, 선택된 순서뒤의 내용이라면 열려있고)
+	var thisView =  $(this).next('dd').css('display');
+
+	if( thisView === 'none' ){		accSt01_dd.slideUp(); 	}
+
+	// 선택된 dt 바로 뒤에있는 dd('accSt01_dd')를 나타나게(slideDown) 해라
+	$(this).next('dd').slideDown();
+}); 
+
 
 
 // jquery end =======================
