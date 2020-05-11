a5248ac16cb2dbe3676cf3d5ebe858edc08759dc
xido <erivery2@gmail.com>
2020-05-11 17:00:46 +0900

    아코디언메뉴구조활용 탭메뉴 레이아웃

--- b_coding/k_jquery/js/src/jq_rel_display.js
+++ b_coding/k_jquery/js/src/jq_rel_display.js
@@ -28,23 +28,31 @@ var accSt01 = accMenu.children('.acc_style_01');
 var accSt01_dt = accSt01.find('dt');
 var accSt01_dd = accSt01.find('dd');
 
-// console.log( accSt01_dt.length, accSt01_dd.length );
+accSt01_dt.find('button').on('click', function(e){
+	e.preventDefault();
+	var dtPar = $(this).parent('dt');
+	var thisPar = dtPar.next('dd');
+	var thisView =  thisPar.css('display') == 'none';
 
-// dt('accSt01_dt')를 클릭시(버튼을 클릭하는 것으로 설정/focus처리)
-accSt01_dt.find('button').on('click focus', function(){
-	// 이미 열려있는 dd는 접히고,(단, 선택된 순서뒤의 내용이라면 열려있고)
-	var thisView =  $(this).parent('dt').next('dd').css('display');
-
-	if( thisView === 'none' ){		accSt01_dd.stop().slideUp(); 	}
-
-	// 선택된 dt 바로 뒤에있는 dd('accSt01_dd')를 나타나게(slideDown) 해라
-	$(this).parent('dt').next('dd').stop().slideDown();
+	if( thisView ){		
+		accSt01_dd.stop().slideUp(); 	
+		thisPar.stop().slideDown();
+		dtPar.addClass('action');
+		dtPar.siblings('dt').removeClass('action'); 	
+	}else{
+		accSt01_dd.stop().slideUp(); 	
+		accSt01_dt.removeClass('action');
+	}
 }); 
 
-// 몇가지 문제점 정리....
-// 1. slide 처리시 toggle이 중복현상
-// 2. focus 처리시 색상처리
-// 3. 변수 정리
+// ======================================
+// 아코디언메뉴 html 구조를 이용한 탭메뉴 만들기
+
+
+
+
+
+
 
 // jquery end =======================
 })(jQuery);
