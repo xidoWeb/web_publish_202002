afc8e7e2bbc689abdc8c74d5fffb0b40e7c8ff72
xido <erivery2@gmail.com>
2020-05-11 14:15:21 +0900

    bg color set

--- b_coding/k_jquery/js/src/jq_rel_display.js
+++ b_coding/k_jquery/js/src/jq_rel_display.js
@@ -8,17 +8,26 @@
  var con  = $('.content');
  var conLi = con.find('div');
 
- st01Li.eq(0).addClass('action');
+//  st01Li.eq(0).addClass('action');
+
+ var liBg = st01Li.eq(0).css('backgroundColor');
+	var c01 = conLi.eq(0).css('backgroundColor');
+	st01Li.eq(0).css({'backgroundColor':c01});
 
  st01Li.on('click', function(e){
 	e.preventDefault();
-	$(this).addClass('action');
-	$(this).siblings().removeClass('action');
+	// $(this).addClass('action');
+	// $(this).siblings().removeClass('action');	
 	
 	var i = $(this).index();
 	// conLi.hide();
 	conLi.eq(i).siblings().stop().hide();
 	conLi.eq(i).stop().fadeIn();
+
+	var conCol = conLi.eq(i).css('backgroundColor');
+	$(this).siblings().css({'backgroundColor':liBg});
+	$(this).css({'backgroundColor':conCol});
+
  });
 
 
