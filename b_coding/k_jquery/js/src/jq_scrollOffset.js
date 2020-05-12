4a25470684a7474644ed535804b733a2ce640e71
xido <erivery2@gmail.com>
2020-05-12 13:12:47 +0900

    scrollTop and parallax

--- b_coding/k_jquery/js/src/jq_scrollOffset.js
+++ b_coding/k_jquery/js/src/jq_scrollOffset.js
@@ -20,21 +20,46 @@
 // offset() : 브라우서 상단에서 부터 원하는 개체(선택자)가 얼만큼 떨어져 있는지를 확인하는 기능
 // offset().top   |  offset().left
 
-// $(document).on('scroll', function(){
-// 	var doScroll = $(this).scrollTop();
-// 	var doOffset =$('#headBox').offset();
-	
-// 	console.log( doScroll, doOffset );
+var headBox = $('#headBox');
+var hT =  headBox.offset().top;
 
-// 	$('#headBox').offset({top:doScroll, left: doScroll});
-// });
+$(document).on('scroll', function(){
+	var docST = $(document).scrollTop(); // 스크롤의 위치 파악
 
+	if(docST > hT){
+		headBox.css({'position':'fixed', 'top':'0'});
+	}else{
+		headBox.removeAttr('style');
+	}
+});
 
 // =====================================
 var bodyH = $('body').height();
 // console.log(bodyH);
 $('#viewBox').height(bodyH);
 
+// scrollTop 응용 ====================================
+var viewBox = $('#viewBox');
+var viewBoxH2 = viewBox.children('h2');
+var viewVideo = viewBox.find('.video');
+var titleBg   = $('.title_bg');
+
+var conBgP = $('.con_img').css('backgroundPositionY');
+var parseCon = parseInt( conBgP );
+
+$(document).on('scroll', function(){
+	var doScroll = $(document).scrollTop();
+	
+	viewBoxH2.css({'marginTop':doScroll/2+'px'});
+	viewVideo.css({'marginTop':doScroll * 1.2  + 'px'});
+	
+	var ot = -doScroll/3 ;
+	titleBg.css({'marginTop':ot * 0.7 + 'px'});
+	titleBg.children('h2').css({'marginTop':ot * 0.4 + 'px'});
+
+	$('.con_img').css({'backgroundPositionY':parseCon - (ot/15) + '%'});
+
+});
 
 // jquery end
 })(jQuery);
