9c7f2a6c714d012d761cdb3704cc87583b0be13d
xido <erivery2@gmail.com>
2020-05-14 16:06:26 +0900

    indicator more set

--- b_coding/k_jquery/js/src/jq_eq_index_setInterval.js
+++ b_coding/k_jquery/js/src/jq_eq_index_setInterval.js
@@ -1,5 +1,21 @@
 // jq_eq_index_setInterval.js
-
 (function($){
-	
-}(jQuery);
+
+	var indi = $('.indicator');
+	var indiLi = indi.find('li');
+	var indiLink = indiLi.children('a');
+	var addView  = $('.add_view_wrap');
+	var timed    = 500;
+
+	indiLink.on('focus', function(e){
+		e.preventDefault();
+		var i = $(this).parent('li').index();
+		// console.log(i);
+		var myPer = -i * 100;
+
+		// addView.css({'marginLeft':myPer + '%'});
+		addView.stop().animate({'marginLeft':myPer + '%'}, timed);
+	});
+
+
+})(jQuery);
