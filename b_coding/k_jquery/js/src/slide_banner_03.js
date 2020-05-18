ca09a00900c34a37b986a7fe4ab2254871eb12ff
xido <erivery2@gmail.com>
2020-05-18 13:22:20 +0900

    좌우버튼 통합1 , span기능 일부 수정

--- b_coding/k_jquery/js/src/slide_banner_03.js
+++ b_coding/k_jquery/js/src/slide_banner_03.js
@@ -1,8 +1,16 @@
 // slid_banner_03.js
 
 var miniProduct = $('#miniProduct');
+var miniBtn     = miniProduct.find('button');
 var miniBtnNext = miniProduct.find('.next');
 var miniBtnPrev = miniProduct.find('.prev');
+var timed       = 500;
+
+// ----------------------------------------------
+miniBtn.on('click', function(e){
+	var t = $(this)[0];
+	console.log( t === miniBtnNext[0] );
+});
 
 var productUl = miniProduct.find('.product');
 var cloneLi   = productUl.children('li').eq(-1).clone(true);
@@ -19,6 +27,7 @@ console.log(proLiLen);
  var n = 0;
  var check = true;
 
+/*
  miniBtnNext.on('click', function(e){
 	 e.preventDefault();
 	 if(check){
@@ -43,12 +52,49 @@ console.log(proLiLen);
 		 });
 	 }
 });
+*/
+var prSpan = productLi.find('span');
+// prSpan.css({bottom:-100 + '%'});
+// 버튼 기능 합치기
+miniBtn.on('click', function(e){
+	e.preventDefault();
+	var thisBtn = $(this)[0];
+	// next 버튼 클릭시
+	prSpan.animate({bottom:-100 + '%'});
+	if(thisBtn === miniBtnNext[0] && check){
+		check = false;
+		n += 1;
+		productUl.stop().animate({left: -n * 100 + '%'}, timed, function(){
+			if(n >= proLiLen - 2){	n = -1;	 }
+			prSpan.animate({bottom:0});
+
+			productUl.css({left: -n * 100 + '%'});
+			check = true;
+		 });
+	}
+	// prev 버튼 클릭
+	else if(check) {
+		check = false;
+		n -= 1;
+		productUl.stop().animate({left: -n * 100 + '%'}, timed, function(){
+			if(n <= -1){	n = proLiLen - 2;	 }
+			productUl.css({left: -n * 100 + '%'});
+			check = true;
+		 });
+	}
+});
+
+
+
+
+
+
 
 var mvImg;
 var mvSlideGo = function(){
 	mvImg = setInterval(function(){
 		miniBtnNext.trigger('click');
-	}, 2000);
+	}, timed*15);
 };
 
 mvSlideGo();
