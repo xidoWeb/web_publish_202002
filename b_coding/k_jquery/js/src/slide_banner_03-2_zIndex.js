3c32f678fcb79e0193f5ba49b8c7f8076310c0c7
xido <erivery2@gmail.com>
2020-05-19 12:55:44 +0900

    fade slide count

--- b_coding/k_jquery/js/src/slide_banner_03-2_zIndex.js
+++ b_coding/k_jquery/js/src/slide_banner_03-2_zIndex.js
@@ -1,4 +1,5 @@
 // slide_banner_03-2_zIndex.js
+(function($){
 
 var productWrap = $('.product_wrap');
 var product = $('.product');
@@ -14,11 +15,13 @@ productLi.css({width:'100%', position:'absolute', left:0, top:0});
 // .indicator_color 생성
 var indicatorColorCode = '<div class="indicator_color hidden_wrap"><ul></ul></div>';
 var indiLiCode = '<li><a href="#"><span></span></a></li>';
-
+// productWrap.before('<div class="indicator_color hidden_wrap"><ul></ul></div>');
 		productWrap.before(indicatorColorCode);
+
 var indiCatorColorUl = $('.indicator_color').find('ul');
 
 var i = 0;
+var n = 0;
 var proNarr;
 
 for(; i < productLi.length; i += 1 ){
@@ -32,39 +35,64 @@ var indicatorColorStyle = '.indicator_color {\
 														 display:inline-block; width:100%; height:30px; text-align:center; \
 														 position:absolute; right:0; bottom:-50px; z-index:500; \
 														}\
-													 .indicator_color ul {\
+													  .indicator_color ul {\
 													   float:left; width:auto; height:100%; padding:0 10px;\
-													 }\
-													 .indicator_color li {\
+													  }\
+													  .indicator_color li {\
 													  float:left; margin-right:0.5rem; width:1.5rem; height:1.5rem; background-color:#fa0;\
-													 }\
-													 .indicator_color li.action{ background-color:#0af; }\
-													 .indicator_color a{ display:block; width:100%; height:100% }';
+													  }\
+													  .indicator_color li.action{ background-color:#0af; }\
+													  .indicator_color a{ display:block; width:100%; height:100% }';
 
 $('head').append('<style>'+ indicatorColorStyle +'</style>');
 // $('style').append();
 // -------------------------------------------------
-$('.indicator_color').append('<p><span class="n_count">1</span> / <span class="t_count">5</span></p>');
-
+$('.indicator_color').append('<p><span class="n_count">3</span> / <span class="t_count">3</span></p>');
 var indicatorColorCount = '.indicator_color p {float:right; width:40px;}';
 $('head').find('style').append(indicatorColorCount);
 // ----------------------------------------------------
-var n = 0;
 
-productLi.eq(n).css({zIndex:10});
-productLi.eq(n).siblings('li').css({zIndex:0, display:'none'});
+// productLi.eq(n).css({zIndex:10});
+// productLi.eq(n).siblings('li').css({zIndex:0, display:'none'});
+
+// $('.n_count').text( n+1 );
+$('.t_count').text( productLi.length );
 
 var bool = true;
 
 var FadeFn = function(n,k){
-	productLi.eq(n).css({zIndex:5, display:'block'});
+	if( k !== n ){
+		$('.n_count').text( n+1 );
+		productLi.eq(n).css({zIndex:5, display:'block'});
+		indiLi.eq(n).addClass('action');
+		indiLi.eq(n).siblings().removeClass('action');
+
 		productLi.eq(k).stop().fadeOut( function(){
 			productLi.eq(n).css({zIndex:10});
 			productLi.eq(n).siblings('li').css({zIndex:0});
 			bool = true;
-	});
+		});
+	}
 };
 
+// 인디케이터 클릭 ---------------------------------------------------
+var indiLi = indiCatorColorUl.children('li');
+// indiLi.eq(n).addClass('action');
+
+indiLi.find('a').on('click', function(e){
+	e.preventDefault();
+	k = n;
+	n = $(this).parent().index();
+
+	FadeFn(n, k);
+
+});
+
+
+
+FadeFn(n, 1);
+
+// 좌우 버튼 클릭  --------------------------------------------------------
 productBtn.on('click', function(e){
 	e.preventDefault();
 	var k = n;
@@ -83,3 +111,5 @@ productBtn.on('click', function(e){
 	}
 
 });
+
+})(jQuery);
