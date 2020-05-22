6efd9658ca459ff84cf13750ba0ae0d4eb24f8ec
xido <erivery2@gmail.com>
2020-05-22 11:31:16 +0900

    device list check

--- b_coding/k_jquery/js/src/browser_resize.js
+++ b_coding/k_jquery/js/src/browser_resize.js
@@ -1,21 +1,27 @@
 (function($){
 
-
 	var m1 = $('.motion_01');
 	var m2 = $('.motion_02');
 
-	var MotionM1 = function(){
+	var MotionM1 = function(myC){
+		var myColor;
+		(myC  == undefined) ? myColor = '#078' : 	myColor = myC;
+		// var myColor = myC | '#078';
+
 		m1.on('mouseenter', function(){
-			$(this).animate({'backgroundColor':'#078'});
-			$(this).append('<p>내용이 추가로 담아졌습니다.</p>');
+			$(this).stop().animate({'backgroundColor':myColor});
+			// $(this).empty();
+			$(this).html('<p>내용이 추가로 담아졌습니다.</p>');
 		});
+		console.log(myColor);
 	};
 	
+	var deviceName = ["mobile","tablet","laptop","pcfull"];
 	var deviceWidth = [
-		{ 'size' : 640 ,  'title'  : "mobile"},
-		{ 'size' : 1280 , 'title'  : "tablet"},
-		{ 'size' : 1600 , 'title'  : "laptop"},
-		{ 'size' : 2560 , 'title'  : "pcfull"}
+		{ 'size' : 640 ,  'title'  : deviceName[0]},
+		{ 'size' : 1280 , 'title'  : deviceName[1]},
+		{ 'size' : 1600 , 'title'  : deviceName[2]},
+		{ 'size' : 2560 , 'title'  : deviceName[3]}
 	];
 
 	var win = $(window);
@@ -38,19 +44,29 @@
 	beforeDevice = DeviceAction(ww);
 	// console.log( '1. ' + beforeDevice + '---' );
 
-
-
-
-
-
-	win.on('resize', function(){
+// ------------------------------------------
+	var ActiveStyle = function( widthSize ){
+		if(widthSize  < deviceWidth[0].size ){			      
+			MotionM1();
+		}else if( widthSize < deviceWidth[1].size ){			
+			MotionM1('#ccc');
+		}else if( widthSize < deviceWidth[2].size ){			
+			MotionM1('#076');
+		}else{			                                      
+			MotionM1('#ffc');
+		}
+	};
+	// ActiveStyle();
+// ------------------------------------------
+  win.on('resize', function(){
 		var nw = win.outerWidth(true);
 		afterDevice = DeviceAction(nw);
 		if(beforeDevice !== afterDevice){
 			// 디바이스 상황에따른 변경내용을 수행
-
+			var i = deviceName.indexOf(afterDevice);
+			console.log(i);
+			ActiveStyle(deviceWidth[i].size);
 			beforeDevice = afterDevice;
 		}
 	});
-
 })(jQuery);
