a16dd3f2b97177c083a317e0c14733c9bc6607f0
xido <erivery2@gmail.com>
2020-05-22 11:47:27 +0900

    사이즈별 기능 구분

--- b_coding/k_jquery/js/src/browser_resize.js
+++ b_coding/k_jquery/js/src/browser_resize.js
@@ -1,8 +1,8 @@
 (function($){
-
+	
+// 상황에 따른 기능 처리 --------------------------------
 	var m1 = $('.motion_01');
 	var m2 = $('.motion_02');
-
 	var MotionM1 = function(myC){
 		var myColor;
 		(myC  == undefined) ? myColor = '#078' : 	myColor = myC;
@@ -16,7 +16,9 @@
 		});
 		console.log(myColor);
 	};
-	
+// ------------------------------------------------------
+
+// 디바이스 환경 변수로 세팅 --------------------------------
 	var deviceName = ["mobile","tablet","laptop","pcfull"];
 	var deviceWidth = [
 		{ 'size' : 640 ,  'title'  : deviceName[0]},
@@ -24,6 +26,10 @@
 		{ 'size' : 1600 , 'title'  : deviceName[2]},
 		{ 'size' : 2560 , 'title'  : deviceName[3]}
 	];
+// -------------------------------------------------------
+
+// 브라우저의 크기 파악 및 기능을 
+// beforeDevice / afterDevice 명칭에 담아 이후에도 동작가능하도록 처리 --
 
 	var win = $(window);
 	var ww  = win.outerWidth(true);
@@ -46,6 +52,8 @@
 	// console.log( '1. ' + beforeDevice + '---' );
 
 // ------------------------------------------
+
+// 현재 디바이스의 크기값 파악 및 디바이스 상황에 맞는 기능수행 ---------
 	var ActiveStyle = function( widthSize ){
 		if(widthSize  < deviceWidth[0].size ){			      
 			MotionM1();
@@ -60,6 +68,8 @@
 	var i = deviceName.indexOf(beforeDevice);
 	ActiveStyle(deviceWidth[i].size);
 // ------------------------------------------
+
+// 브라우저의 사이즈 변경시 위 기능들 수행내용 ---------------------------
   win.on('resize', function(){
 		var nw = win.outerWidth(true);
 		afterDevice = DeviceAction(nw);
@@ -72,4 +82,6 @@
 			console.log( 'changed : ' + beforeDevice );
 		}
 	});
+// ---------------------------------------------
+
 })(jQuery);
