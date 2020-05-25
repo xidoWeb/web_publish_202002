0a9139ef0f57ccb53631a5713fb0e04acfcf1a52
xido <erivery2@gmail.com>
2020-05-25 11:43:53 +0900

    gnb list array set

--- c_exam/coding/js/src/monfee.js
+++ c_exam/coding/js/src/monfee.js
@@ -19,7 +19,7 @@
 		width:'100%', height:headBoxH, backgroundColor:headBoxBg,
 		position:'fixed', top:0, left:0, zIndex:1000
 	});
-	headBox.css({maxWidth:'940px', margin:'auto', backgroundColor:'transparent'});
+	headBox.css({width:'940px', margin:'auto', backgroundColor:'transparent'});
 
 	// ---------------------------
 	 var bestNewsBox = $('#bestNewsBox');
@@ -31,5 +31,41 @@
 	 bestNewsBoxWrap.css({width:'100%', backgroundColor:bestNewsBoxBg});
 	 bestNewsBox.css({width:'940px', backgroundColor:'transparent'});
 
-	// ---------------------------
+var gnbArr = [
+	 {title: 'story',
+	  subList : [
+		  {linkName : '몽피이야기', link : 'storyMonfee.html'},
+	    {linkName : '회사소개', link : 'aboutCompany.html' },
+	    {linkName : '후원업체',  link : 'sponCompany.html'}
+	 ]},
+	 {title: 'menu',
+	  subList : [
+		  {linkName : '커피류', link : 'storyMonfee.html'},
+	    {linkName : '기타음료', link : 'aboutCompany.html' },
+	    {linkName : '음식 및 디저트',  link : 'sponCompany.html'},
+	    {linkName : '선물용 상품',  link : 'sponCompany.html'},
+	    {linkName : '기타 제품',  link : 'sponCompany.html'}
+	 ]},
+	 {title: 'store',
+	  subList : [
+		  {linkName : '신규매장', link : 'storyMonfee.html'},
+	    {linkName : '매장찾기', link : 'aboutCompany.html' },
+	    {linkName : '창업설명회',  link : 'sponCompany.html'},
+	    {linkName : '창업스토리',  link : 'sponCompany.html'}
+	 ]},
+	 {title: 'news',
+	  subList : [
+		  {linkName : '이벤트', link : 'storyMonfee.html'},
+	    {linkName : '공지사항', link : 'aboutCompany.html' },
+	    {linkName : '프로모션',  link : 'sponCompany.html'},
+	    {linkName : '미디어광고',  link : 'sponCompany.html'}
+	 ]}
+	];
+// ----------------------------
+var gnbBox = $('#gnbBox');
+gnbBox.append('<ul class="gnb_area"></div>');
+
+
+
+// ---------------------------
 })(jQuery);
