075c99fc015f37d09bea534f31dc3a39db9f25b6
xido <erivery2@gmail.com>
2020-05-27 11:19:39 +0900

    adbanner 좌우 버튼부

---
+++ c_exam/coding/js/src/monfee_main_adBanner.js
@@ -0,0 +1,45 @@
+// monfee_main_adBanner.js
+
+(function($){
+	var adBannerBox = $('#adBannerBox')	;
+	var adBannerBtn = adBannerBox.find('.adBanner_btn');
+	var adBtn       = adBannerBtn.find('button');
+	var adBannerIndiCator = adBannerBox.find('.adBanner_indicator');
+	var indiCatorLi = adBannerIndiCator.find('li');
+	var adBannerImg  = adBannerBox.find('.adBanner_img');
+	var adBannerImgWrap = adBannerImg.children('ul') ;
+	var adBannerLi     = adBannerImgWrap.find('li');
+	var adBannerLiLen  = adBannerLi.length;
+
+	// common
+	var n = 0;
+
+	// button
+	adBtn.on('click', function(e){
+		e.preventDefault();
+		var clickIt = $(this)[0];
+
+		if( clickIt === $('.next')[0] ){
+			// 다음버튼 클릭
+			n += 1;
+			adBannerImgWrap.stop().animate({'marginLeft':n * -100 + '%'}, function(){
+				if( n >= adBannerLiLen -1){
+					n = -1;
+				}
+			});
+		}else{
+			// 이전버튼 클릭
+			n -= 1;
+			if(n < 0){
+				n = adBannerLiLen -1;
+			}
+			adBannerImgWrap.stop().animate({'marginLeft':n * -100 + '%'});
+		}
+	});
+
+	// indicator
+
+
+
+
+})(jQuery);
