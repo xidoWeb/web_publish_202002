7e4384275561a6f0bac61e9de1a77fe1d0cd26cf
xido <erivery2@gmail.com>
2020-05-29 12:49:43 +0900

    gallery page set1,2,3(layout)

--- b_coding/k_jquery/js/src/gallery.js
+++ b_coding/k_jquery/js/src/gallery.js
@@ -1,4 +1,4 @@
-(function($){
+// (function($){
 
 	// gallery 페이지 만들기
 	// 1. html문서에서 큰 이미지의 경로/파일을 직접 입력해서 가져오게 하기
@@ -7,7 +7,7 @@
 	var bigRec = part01.find('.big_rec');
 	var part01Li = part01.find('li');
 	var part01LiLink = part01Li.children('a');
-	var bigSource = '../../img/gallery/big/';
+	var bigSource = '../img/gallery/big/';
 	
 	part01LiLink.on('click', function(e){
 		e.preventDefault();
@@ -17,11 +17,55 @@
 		bigRec.css({backgroundImage:'url('+ bigSource + thisImg +')'});
 	});
 	// ------------------------------------------------------------
-  // 2. js를 통해 경로를 입력해서 처리하게 만들기
+	// 2. js를 통해 경로를 입력해서 처리하게 만들기
+		var thumList = [
+			'gallery_01.jpg',
+			'gallery_02.jpg',
+			'gallery_03.jpg',
+			'gallery_04.jpg',
+			'gallery_05.jpg'
+		];
+		var bigList = [
+			'gallery_01.jpg',
+			'gallery_02.jpg',
+			'gallery_03.jpg',
+			'gallery_04.jpg',
+			'gallery_05.jpg'
+		];
+	var thumUrl = '../img/gallery/thum/';
+	var bigUrl = '../img/gallery/big/';
+
+	var galleryList = {
+		thumUrl : thumUrl,
+		thumList : thumList,
+		bigUrl  : bigUrl,
+		bigList  : bigList
+	};
+
+	var part02 = $('.part_02');
+	var partRec = part02.find('.big_rec');
+	var thumImg = part02.find('.thum_img').children('ul');
+
+	var i=0;
+	for(; i<thumList.length; i+=1){
+		thumImg.append('<li><a href="#"><span></span></a></li>');
+		// thumImg.find('li').eq(i).children('a').css({backgroundImage:'url('+thumUrl+thumList[i]+')'});
+		thumImg.find('li').eq(i).children('a')
+					 .css({backgroundImage:'url('+thumUrl+galleryList.thumList[i]+')'});
+
+	}
 
 
+	var p2thumLi = thumImg.find('li');
+	var p2thumLiLink = p2thumLi.find('a');
+	// partRec.css({backgroundImage:'url('+bigUrl+galleryList.bigList[0]+')'});
 
+	p2thumLiLink.on('click', function(e){
+		e.preventDefault();
+		var itIndex = $(this).parent('li').index();
+		partRec.css({backgroundImage:'url('+bigUrl+galleryList.bigList[itIndex]+')'});
+	});
 
-
-	
-})(jQuery);
+	p2thumLi.eq(0).find('a').trigger('click');
+ 
+// })(jQuery);
