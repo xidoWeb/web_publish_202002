e101668f4858bcde65f097bd4c787d50d463116d
xido <erivery2@gmail.com>
2020-06-05 14:59:56 +0900

    check 상태 확인 및 문제점 파악

--- c_exam/coding/js/src/join_check.js
+++ c_exam/coding/js/src/join_check.js
@@ -1,4 +1,5 @@
 (function($){
+	var ac = $('#allCheck');
 	var ts = $('#termsService');
 	var tp = $('#termsPrivacy');
 	var tl = $('#termsLocation');
@@ -25,9 +26,44 @@ tsTerms.load('../terms/termsService.html');
 tpTerms.load('../terms/termsPrivacy.txt');
 tlTerms.load('../terms/termsLocation.txt');
 taTerms.load('../terms/termsAlert.txt');
-// =========================================================
+// =======================================================
+var inputCheckList = [ts, tp, tl, ta];
+
+ac.on('click', function(e){
+	// e.preventDefault();
+	var acAttr = ac.is(':checked');
+	if(acAttr){
+		// ac.attr({'checked': 'checked'});
+		ac.attr({'checked': true});
+
+		ts.attr({'checked':true});
+		tp.attr({'checked':true});
+		tl.attr({'checked':true});
+		ta.attr({'checked':true});
+
+	}else{
+
+		// ac.removeAttr('checked');
+		ac.attr({'checked': false});
+
+		ts.attr({'checked': false});
+		tp.attr({'checked': false});
+		tl.attr({'checked': false});
+		ta.attr({'checked': false});
+	}
+	
+	console.log(acAttr);
+});
 
 
+$('input[type="checkbox"]').on('click', function(){
+	// console.log( $(this) );
+	var nowCk = $(this).is(':checked');
+	if(nowCk === false){
+		ac.attr({'checked': false});
+		// ac.removeAttr({'checked': false});
+	}
+});	
 
 
 })(jQuery);
