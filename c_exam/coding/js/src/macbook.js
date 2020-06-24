adc6b20e78b4c6887e47f72d505eca318d9e1c08
xido <erivery2@gmail.com>
2020-06-24 12:15:53 +0900

    open laptop image

--- c_exam/coding/js/src/macbook.js
+++ c_exam/coding/js/src/macbook.js
@@ -5,24 +5,52 @@
 	var macbook      = openLaptop.find('.macbook');
 	var macbookInImg = '<img src="" alt="맥북이미지" />';
 	var url          = '../img/mac/';
-	var imgName, j;
+	var j='0000';
+	var imgLength = 93;
 
-	for(var i=0; i<93; i++){
+	for(var i=0; i<imgLength; i++){
 		
-		if(i)
+		if(i < 10){
+			j = '000' + i;
+		}else if(i < 100){
+			j = '00' + i;
+		}else if(i < 1000){
+			j = '0' + i;
+		}else{
+			j = i;
+		}
 
 		macbook.append(macbookInImg);
+		macbook.find('img').eq(i).attr({'src':url + 'large_' + j + '.jpg'});
 	}
+	// macbook.find('img').not( macbook.find('img').eq(0)  ).hide();
+	macbook.find('img').hide();
+	macbook.find('img').eq(0).show();
 
-
-
+// -------------------------------------------
+var secondScrollStart = 850;
 
 	win.on('scroll', function(){
 		var thisScroll = $(this).scrollTop();
-		var myop = 1 - thisScroll / 400;
+		// 상단 제목, 스크롤이 이동 + 점점 투명해지게하기
+		var myop = 1 - thisScroll / 400;		
+		openH2.css({top: -thisScroll, opacity:myop});		
+		
+		// 스크롤시 노트북 이미지 변경되게 하기
+		var imgSelect = parseInt( thisScroll / 600 * imgLength );
+		if (imgSelect >= imgLength){
+			imgSelect = imgLength-1;			
+		}
 
-		// console.log(thisScroll);
-		// console.log(myop);
-		openH2.css({top: -thisScroll, opacity:myop});
+		console.log(secondScrollStart);
+
+		macbook.find('img').eq( imgSelect ).show();
+		macbook.find('img').eq( imgSelect ).siblings().hide();
+
+		// 일정 스크롤이 지난시점에서 openLaptop의 위치를 이동처리
+		if(thisScroll >= secondScrollStart){
+			openLaptop.css({top : secondScrollStart - thisScroll});
+		}
+
 	});	
 })(jQuery);
