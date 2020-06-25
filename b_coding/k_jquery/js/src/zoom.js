1d01186628099b183bccad5d803af88431b92745
xido <erivery2@gmail.com>
2020-06-25 11:00:24 +0900

    zoom 기본 기능

--- b_coding/k_jquery/js/src/zoom.js
+++ b_coding/k_jquery/js/src/zoom.js
@@ -1,11 +1,11 @@
 // zoom.js
 (function($){
 	var url        = '../img/gallery/';
-	var galleryImg = [
-		{"big"  : ['gallery_01.jpg', 'gallery_02.jpg', 'gallery_03.jpg']},
-		{"thum" : ['gallery_01.jpg', 'gallery_02.jpg', 'gallery_03.jpg']},
-		{"thumAlt" : ['gallery_01설명', 'gallery_02설명', 'gallery_03설명']},
-	];
+	var galleryImg = {
+		"big"     : ['gallery_01.jpg', 'gallery_02.jpg', 'gallery_03.jpg','gallery_04.jpg'],
+		"thum"    : ['gallery_01.jpg', 'gallery_02.jpg', 'gallery_03.jpg','gallery_04.jpg'],
+		"thumAlt" : ['gallery_01 설명', 'gallery_02 설명', 'gallery_03 설명','gallery_04 설명']
+	};
 
 	var product = $('.product');
 	var big     = product.find('.big');
@@ -15,13 +15,49 @@
 	thum.append('<ul></ul>');
 	var thumUl = thum.find('ul');
 	var i=0; 
-	var thumLength = galleryImg[1].thum.length;
+	var thumLength = galleryImg.thum.length;
 	console.log( thumLength );
+
 	for(; i<thumLength; i++){
 		thumUl.append('<li><a href="#"><span></span></a></li>');
 		thumUl.find('li').eq(i).children('a').css({
-			backgroundImage:'url(' + url +'thum/'+ galleryImg[1].thum[i] +')'
+			backgroundImage:'url(' + url +'thum/'+ galleryImg.thum[i] +')'
 		});
 	};
+
+	var index = 0;
+
+	big.css({backgroundImage:'url(' + url +'big/'+ galleryImg.big[ index ] +')'});
+
+	var thumLi = thumUl.find('li');
+	thumLi.eq( index ).children('a').focus();
+
+	thumLi.children('a').on('click', function(e){
+		e.preventDefault();
+		index = $(this).parent('li').index();
+
+		big.css({backgroundImage:'url(' + url +'big/'+ galleryImg.big[ index ] +')'});
+	});
+
+	var baseWidth = big.innerWidth();
+	var baseHeight = big.innerHeight();
+	
+	big.on('mousemove', function(e){
+		//좌표위치
+		var evtLocationX = e.originalEvent.offsetX;
+		var evtLocationY = e.originalEvent.offsetY;
+
+		// 좌표위치 %체크
+		var xPer = parseInt(evtLocationX / baseWidth * 100);
+		var yPer = parseInt(evtLocationY / baseHeight * 100);
+		// console.log(xPer,	yPer);
+
+		zoom.css({
+			backgroundImage:'url(' + url +'big/'+ galleryImg.big[ index ] +')',
+			backgroundPosition: xPer + '%' + '  ' + yPer + '%'
+		});
+
+
+	});
 
 })(jQuery);
