eb317c8e0f1bfdba8846a20258edde3fbbd9ed6c
xido <erivery2@gmail.com>
2020-07-01 12:24:48 +0900

    json data 불러와서 설정

--- b_coding/k_jquery/js/src/moreView.js
+++ b_coding/k_jquery/js/src/moreView.js
@@ -4,12 +4,30 @@
 	var sampleCode = '<div class="person">\
 											<div class="img"><img src="" alt=""></div>\
 											<dl>\
-												<dt>name</dt><dd>xido lee</dd>\
-												<dt>email</dt><dd>xidoweb@gmail.com</dd>\
+												<dt>name</dt><dd class="peopleName">xido lee</dd>\
+												<dt>email</dt><dd class="email">xidoweb@gmail.com</dd>\
 											</dl>\
 										</div>';
 
-  content.append(sampleCode);
+  // content.append(sampleCode);
+
+	$.ajax({
+		url:"../data/people.json",
+	}).done(function(data){
+		console.log(data);
+		var i=0;
+		var person ;
+		for(; i<20; i++){
+			content.append(sampleCode);
+			person = $('.person');
+			person.eq(i).find('.peopleName').text( data[i].name );
+			person.eq(i).find('.email').text( data[i].email );
+		}
+
+
+
+	});
+
 
 
 })(jQuery);
