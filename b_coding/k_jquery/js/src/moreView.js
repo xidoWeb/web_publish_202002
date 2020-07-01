81605f486c183a3a527b06bb43714fd6fb09de9f
xido <erivery2@gmail.com>
2020-07-01 13:14:06 +0900

    더보기 버튼 클릭 내용 설정하기

--- b_coding/k_jquery/js/src/moreView.js
+++ b_coding/k_jquery/js/src/moreView.js
@@ -1,7 +1,8 @@
 (function($){
 	var content = $('.content');
+	var moreBtn    = $('.more').find('button');
 
-	var sampleCode = '<div class="person">\
+	var sampleCode = '<div class="person"><span class="num"></span>\
 											<div class="img"><img src="" alt=""></div>\
 											<dl>\
 												<dt>name</dt><dd class="peopleName">xido lee</dd>\
@@ -14,17 +15,31 @@
 	$.ajax({
 		url:"../data/people.json",
 	}).done(function(data){
-		console.log(data);
 		var i=0;
-		var person ;
-		for(; i<20; i++){
-			content.append(sampleCode);
-			person = $('.person');
-			person.eq(i).find('.peopleName').text( data[i].name );
-			person.eq(i).find('.email').text( data[i].email );
-		}
+		var person;
+		var firstView = 20;
+		var myView = 10;
+		var maxN   = 30;
 
+		var LoadData = function(n){
+			// if(n > 0){ k=n }else{k = myView}
+			var k = n || myView;
+			var j = i + k;		
+			for(; i<j; i++){			
+				content.append(sampleCode);
+				person = $('.person');
+				person.eq(i).find('span.num').text( i + 1 );
+				person.eq(i).find('.peopleName').text( data[i].name );
+				person.eq(i).find('.email').text( data[i].email );
+			}
+			if(i >= maxN){ moreBtn.hide() }
+		}; // LoadData(n);
+		LoadData(firstView);
 
+		moreBtn.on('click', function(e){
+			e.preventDefault();
+			LoadData();
+		});
 
 	});
 
