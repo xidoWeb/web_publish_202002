69fca98826166ee8f0f605e9094881db2505da29
xido <erivery2@gmail.com>
2020-07-01 12:09:33 +0900

    more 버튼 이요하여 더보기 기능 이해하기

---
+++ b_coding/k_jquery/js/src/moreView.js
@@ -0,0 +1,15 @@
+(function($){
+	var content = $('.content');
+
+	var sampleCode = '<div class="person">\
+											<div class="img"><img src="" alt=""></div>\
+											<dl>\
+												<dt>name</dt><dd>xido lee</dd>\
+												<dt>email</dt><dd>xidoweb@gmail.com</dd>\
+											</dl>\
+										</div>';
+
+  content.append(sampleCode);
+
+
+})(jQuery);
