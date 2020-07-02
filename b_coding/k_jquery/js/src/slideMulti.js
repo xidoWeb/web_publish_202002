72087780f6e8b068256d999cbc1e0e9df9ba5072
xido <erivery2@gmail.com>
2020-07-02 17:46:40 +0900

    multi slide

--- b_coding/k_jquery/js/src/slideMulti.js
+++ b_coding/k_jquery/js/src/slideMulti.js
@@ -14,27 +14,29 @@ slideLi     = slideUl.find('li');
 slideLi.eq(1).addClass('active');
 
 
-// --------------------------------------
-// 버튼 클릭시 내용 변경1
-var hasActive , q;
-var btnTrue = true;
-
-btn.on('click', function(e){
-	e.preventDefault();
-	
-	var nBtn = $(this)[0] === $('.next')[0];
-	
+// 정면에 보이는 배너순서 찾기(.active 찾기)
+var hasActive;
+var q;
+var WhereActive = function(){
   // 정면에 보이는 배너순서 찾기(.active 찾기)
 	for(var i=0; i<slideLi.length; i++){
 		hasActive = slideLi.eq(i).hasClass('active');
 		if(hasActive){q = i;	break;}
 	}
+	return q;
+};
 
+WhereActive();
+// --------------------------------------
+// 버튼 클릭시 내용 변경1
+var btnTrue = true;
+
+btn.on('click', function(e){
+	e.preventDefault();
+	var nBtn = $(this)[0] === $('.next')[0];	
+	WhereActive();
 	slideLi.removeClass('active');
-
-	if( nBtn && btnTrue){ 
-		//next 버튼 클릭시
-
+	if( nBtn && btnTrue){ //next 버튼 클릭시
 		// 다음버튼 중복기능 방지
 		btnTrue = false;
 		// 각 내용 위치이동
@@ -46,6 +48,8 @@ btn.on('click', function(e){
 			transform:'translate3D(-100%, 0, 0) scale(0.8) rotateY(45deg)',zIndex:-1,
 			transition:'all 500ms linear'
 		});
+		slideLi.addClass('up');
+
 		slideLi.eq(q+1).css({
 			transform:'translate3D(0, 0, 0) scale(1) rotateY(0)',	zIndex:100,
 			transition:'all 500ms linear'
@@ -54,6 +58,7 @@ btn.on('click', function(e){
 			slideLi.eq(0).appendTo(slideUl);		
 			slideLi     = slideUl.find('li');
 			slideLi.eq(1).addClass('active');
+			slideLi.removeClass('up');
 			btnTrue = true;
 		}, 500);
 
@@ -70,6 +75,8 @@ btn.on('click', function(e){
 				transform:'translate3D(100%, 0, 0) scale(0.8) rotateY(-45deg)',zIndex:-1,
 				transition:'all 500ms linear'
 			});
+			slideLi.addClass('up');
+
 			slideLi.eq(q+1).css({
 				transform:'translate3D(-100%, 0, 0) scale(0.8) rotateY(45deg)',
 				transition:'all 500ms linear'
@@ -78,14 +85,29 @@ btn.on('click', function(e){
 				slideLi.eq(-1).prependTo(slideUl);		
 				slideLi     = slideUl.find('li');
 				slideLi.eq(1).addClass('active');
+				slideLi.removeClass('up');
 				btnTrue = true;
 			}, 500);
 	}
-	
-
 });
 
 
+// -------------------------------------------
+// 카드에 마우스 올렸을때 움직임처리
+// console.log(q);
+
+slideLi.eq(q).on('mousemove', function(e){
+	// 마우스 위치파악
+	var mouseX = e.originalEvent.offsetX;
+	var cardHalfWidth = $(this).outerWidth() / 2;
+
+	if(mouseX <= cardHalfWidth){
+		$(this).css({transform:'rotateY(-15deg)', transition:'all 300ms ease'});
+	}else{
+		$(this).css({transform:'rotateY(15deg)', transition:'all 300ms ease'});
+	}
+	
+});
 
 
 
