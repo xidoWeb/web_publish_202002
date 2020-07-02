fd6219f0dcbbc8eabd1261c1a5dd4b9d78170ef0
xido <erivery2@gmail.com>
2020-07-02 16:02:43 +0900

    multi slide 움직임 파악

--- b_coding/k_jquery/js/src/slideMulti.js
+++ b_coding/k_jquery/js/src/slideMulti.js
@@ -1,5 +1,65 @@
 // slideMulti.js
 (function($){
-
+// 선택자
+	var slideBanner = $('.slide_banner');
+	var btn         = $('.btn').find('button');
+	var slideUl     = slideBanner.children('ul');
+	var slideLi     = slideUl.find('li');
 	
+// ---------------------------------------
+// 순서 변경, .active삭제 및 재설정
+slideLi.removeClass('active');
+slideLi.eq(-1).prependTo(slideUl);
+slideLi     = slideUl.find('li');
+slideLi.eq(1).addClass('active');
+
+
+// --------------------------------------
+// 버튼 클릭시 내용 변경1
+var hasActive , q;
+
+btn.on('click', function(e){
+	e.preventDefault();
+	var nBtn = $(this)[0] === $('.next')[0];
+
+  // 정면에 보이는 배너순서 찾기(.active 찾기)
+	for(var i=0; i<slideLi.length; i++){
+		hasActive = slideLi.eq(i).hasClass('active');
+		if(hasActive){
+			q = i;
+			break;
+		}
+	}
+
+	if( nBtn ){ 
+		//next 버튼 클릭시
+		slideLi.eq(-1).css({
+			transform:'translateX(0) scale(1) rotateY(0)',	zIndex:100,
+			transition:'all 500ms ease'
+		});
+
+		slideLi.eq(q).css({
+			transform:'translateX(-100%) scale(0.8) rotateY(45deg)',zIndex:-1,
+			transition:'all 500ms ease'
+		});
+
+		slideLi.eq(0).css({
+			transform:'translateX(100%) scale(0.8) rotateY(-45deg)',zIndex:-1,
+			transition:'all 700ms ease'
+		});
+		
+	}
+	/*else{
+		// prev 버튼 클리기
+
+	}
+	*/
+
+});
+
+
+
+
+
+
 })(jQuery);
