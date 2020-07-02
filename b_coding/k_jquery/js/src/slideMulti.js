cdbd5af26f09a3595666293a47161138ad2eb8d6
xido <erivery2@gmail.com>
2020-07-02 16:52:26 +0900

    multi slide

--- b_coding/k_jquery/js/src/slideMulti.js
+++ b_coding/k_jquery/js/src/slideMulti.js
@@ -17,6 +17,7 @@ slideLi.eq(1).addClass('active');
 // --------------------------------------
 // 버튼 클릭시 내용 변경1
 var hasActive , q;
+var btnTrue = true;
 
 btn.on('click', function(e){
 	e.preventDefault();
@@ -31,33 +32,58 @@ btn.on('click', function(e){
 		}
 	}
 
-	if( nBtn ){ 
+	if( nBtn && btnTrue){ 
 		//next 버튼 클릭시
-		slideLi.eq(0).css({
+
+		// 다음버튼 중복기능 방지
+		btnTrue = false;
+		// 각 내용 위치이동
+		slideLi.eq(q-1).css({
 			transform:'translate3D(100%, 0, 0) scale(0.8) rotateY(-45deg)',
-			transition:'all 800ms linear'
+			transition:'all 500ms linear'
 		});	
 		slideLi.eq(q).css({
 			transform:'translate3D(-100%, 0, 0) scale(0.8) rotateY(45deg)',zIndex:-1,
 			transition:'all 500ms linear'
 		});
-		slideLi.eq(-1).css({
+		slideLi.eq(q+1).css({
 			transform:'translate3D(0, 0, 0) scale(1) rotateY(0)',	zIndex:100,
-			transition:'all 800ms linear'
+			transition:'all 500ms linear'
 		});
 		setTimeout(function(){
 			slideLi.removeClass('active');
 			slideLi.eq(0).appendTo(slideUl);		
 			slideLi     = slideUl.find('li');
 			slideLi.eq(1).addClass('active');
-		}, 1000);
+			btnTrue = true;
+		}, 500);
 
+	}else if(btnTrue){
+		// prev 버튼 클릭
+			// 버튼 중복기능 방지
+			btnTrue = false;
+			// 각 내용 위치이동
+			slideLi.eq(q-1).css({
+				transform:'translate3D(0, 0, 0) scale(1) rotateY(0)',	zIndex:100,
+				transition:'all 500ms linear'
+			});	
+			slideLi.eq(q).css({
+				transform:'translate3D(100%, 0, 0) scale(0.8) rotateY(-45deg)',zIndex:-1,
+				transition:'all 500ms linear'
+			});
+			slideLi.eq(q+1).css({
+				transform:'translate3D(-100%, 0, 0) scale(0.8) rotateY(45deg)',
+				transition:'all 500ms linear'
+			});
+			setTimeout(function(){
+				slideLi.removeClass('active');
+				slideLi.eq(-1).prependTo(slideUl);		
+				slideLi     = slideUl.find('li');
+				slideLi.eq(1).addClass('active');
+				btnTrue = true;
+			}, 500);
 	}
-	/*else{
-		// prev 버튼 클리기
-
-	}
-	*/
+	
 
 });
 
