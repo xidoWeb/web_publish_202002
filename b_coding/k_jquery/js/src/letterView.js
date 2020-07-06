61d21080f24c89287f9126649a1d76b2728db9c1
xido <erivery2@gmail.com>
2020-07-06 10:59:48 +0900

    글자 일정시간뒤 사라지고 다시 만들기

--- b_coding/k_jquery/js/src/letterView.js
+++ b_coding/k_jquery/js/src/letterView.js
@@ -1,44 +1,71 @@
 (function($){
 
 	var inputText1 = $('.text_01');
-	var inputText2 = $('.text_02');
-	
-	var text1Con   = inputText1.text();
-	var text2Con   = inputText2.text();
-	
-	console.log(text1Con);
-	inputText1.empty();
-	inputText2.empty();
-	
+	var text1Con   = inputText1.text();	
+	inputText1.empty();	
 	var t1Arr = text1Con.split('');
-	console.log(t1Arr);
-	
 	var endBox = '<span class="end_box"></span>';
-	var i = 0;
-	var textGo;
-	var StartText = (function(){
-		textGo = setInterval(function(){
-			// 글자 하나씩 나타나게 만들기
-			if(i < t1Arr.length){
-				inputText1.append( t1Arr[i] );
+	// 첫번째 기능
+	(function(){
+		// console.log(text1Con);
+		// console.log(t1Arr);
+		var i = 0;
+		var textGo;
+		var StartText = function(){
+			textGo = setInterval(function(){
+				// 글자 하나씩 나타나게 만들기
+				if(i < t1Arr.length){
+					inputText1.append( t1Arr[i] );
+				}	else if( i == t1Arr.length ){
+					// 마지막에 커서형태 삽입
+					inputText1.append(endBox);
+				}else if( i > t1Arr.length ){
+					// 내용 모두 작성시 종료
+					// clearInterval(textGo);	
+					// 커서가 깜빡거리게 만들기 
+					inputText1.find('.end_box').remove();
+					i = t1Arr.length-1;
+					// console.log(i)
+				}
+				i += 1;
+			},300);
+		};
+		StartText();
+	})();
 
-			}	else if( i == t1Arr.length ){
-				// 마지막에 커서형태 삽입
-				inputText1.append(endBox);
+	// ----------------------------------------------------
+	
+	// 두번째 기능
+	var inputText2 = $('.text_02');
+	var text2Con   = inputText2.text();
+	var t2Arr = text2Con.split('');
+	console.log(t2Arr);
+	inputText2.empty();
 
-			}else if( i > t1Arr.length ){
-				// 내용 모두 작성시 종료
-				// clearInterval(textGo);	
+	(function(){
+		var i = 0;
+		var j = 0;
+		setInterval(function(){
+			if(i < t2Arr.length){
+				inputText2.append(t2Arr[i]);	
+			}else if(i == t2Arr.length){
+				inputText2.append(endBox);
+			}else if(i > t2Arr.length){
+				inputText2.find('.end_box').remove();
+				i = t2Arr.length -1;
 
-				// 커서가 깜빡거리게 만들기 
-				inputText1.find('.end_box').remove();
-				i = t1Arr.length-1;
-				// console.log(i)
+				j += 1;
+				// console.log(j);
+				if(j >= 5){
+					i = -3;
+					j = 0;
+					inputText2.empty();
+				}
 			}
 			i += 1;
-		},300);
+		}, 300);
 	})()
 	
-	
+	//
 
 })(jQuery);
