b0f99c2b7b70a74dd49525d2af813f65387d8423
xido <erivery2@gmail.com>
2020-07-07 13:21:04 +0900

    skill graph image

--- b_coding/j_javascript_01/js/src/canvas_05.js
+++ b_coding/j_javascript_01/js/src/canvas_05.js
@@ -3,26 +3,41 @@ var ctx = canvas.getContext('2d');
 
 var baseColor = "#07a";
 ctx.lineWidth = 15;
+ctx.lineCap = 'round';
 ctx.strokeStyle = baseColor;
 ctx.font = "40px sans-serif";
 ctx.textAlign = 'center';
 ctx.fillStyle = baseColor;
 
-var posX = 200;
-var posY = 200;
-var percent = 70; 
+var MyGraph = function(x, y, p, s){
 
-var myP = function(percent){
-	// percent  :  값 / 기준 * 100 -> 값 / 100 * 기준
-	var p = (percent / 100 * 2) + 1.5;
-	return Math.PI * p;
-};
+	var posX = x || 200;
+	var posY = y || 200;
+	var percent = p || 70; 
+	var skill = s || 'program';
 
-ctx.beginPath();
-ctx.arc(posX,posY,100, Math.PI * 1.5 , myP(percent), false);
-ctx.stroke();
+	var myP = function(percent){
+		// percent  :  값 / 기준 * 100 -> 값 / 100 * 기준
+		var p = (percent / 100 * 2) + 1.5;
+		return Math.PI * p;
+	};
 
-ctx.font = "normal 35px sans-serif";
-ctx.fillText('jQuery', posX, posY);
-ctx.font = "bold 25px sans-serif";
-ctx.fillText(percent+ '%', posX, posY + 35);
+	ctx.beginPath();
+	ctx.arc(posX,posY,100, Math.PI * 1.5 , myP(percent), false);
+	ctx.stroke();
+
+	ctx.font = "bolder 35px sans-serif";
+	ctx.fillText(skill, posX, posY);
+	ctx.font = "bold 25px sans-serif";
+	ctx.fillText(percent+ '%', posX, posY + 35);
+
+}// MyGraph(x좌표, y좌표, percent, 스킬명);
+
+MyGraph(150, 150, 90, 'HTML');
+MyGraph(400, 150, 95, 'CSS');
+MyGraph(650, 150, 95, 'SCSS');
+MyGraph(150, 400, 85, 'Js');
+MyGraph(400, 400, 85, 'jQuery');
+MyGraph(150, 650, 70, 'Photoshop');
+MyGraph(400, 650, 70, 'Illustrator');
+MyGraph(650, 650, 70, 'Indesign');
