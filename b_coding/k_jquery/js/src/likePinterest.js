0f4565327070ce7dc7dc2317a0cb85fc8a4dfc03
xido <erivery2@gmail.com>
2020-07-10 11:47:14 +0900

    like pin 카드 배치 임의 체크

--- b_coding/k_jquery/js/src/likePinterest.js
+++ b_coding/k_jquery/js/src/likePinterest.js
@@ -10,16 +10,18 @@
 	var win = $(window);
 	var wrap = $('#wrap');
 	var cardWrap = $('.card_wrap');
-	var card; //  = $('.card');
 	var cardWidth = 250;
-
+	
 	var winW =	win.outerWidth();
 	var cardRowLength; // 브라우저에서 가로 카드 갯수
-
+	
+	var card; //  = $('.card');
+	var jc;   //  card생성시 순번체크용
+	var beforH;  // 이전라인의 동일순번째의 높이값(top값 포함)
 	// --------------------------------------
 	// 불러올 카드 높이값 설정
 	var cardHeight = [];
-	for(var i=0; i<20; i+=1){
+	for(var i=0; i<100; i+=1){
 		cardHeight[i] = parseInt(Math.random() * 600) + 200;
 	}
 
@@ -29,9 +31,39 @@
 		wrap.css({width:cardRowLength * cardWidth + 'px'});
 		// console.log(cardRowLength);
 		//  -------------------------------
-		for(var j=0; j<20; j+=1){
+		for(var j=0; j<50; j+=1){
 			cardWrap.append(cardCode);
-			cardWrap.find('.card').eq(j).css({height:cardHeight[j]});
+			card = cardWrap.find('.card').eq(j);
+			card.css({height:cardHeight[j]});
+
+			if(j < cardRowLength){ 
+				card.css({top:0, left:cardWidth * j});
+
+			}else if(j < cardRowLength * 2){
+				jc =	j - cardRowLength;
+				beforH = cardWrap.find('.card').eq(jc).outerHeight();
+
+				card.css({top:beforH + 'px', left:cardWidth * jc + 'px'});
+			}else if(j < cardRowLength * 3){
+				jc =	j - cardRowLength;
+				var jc2 = j - cardRowLength *2;
+				beforH = cardWrap.find('.card').eq(jc).outerHeight() +
+								 parseInt( cardWrap.find('.card').eq(jc).css('top') );
+
+				console.log(jc, beforH);
+
+				card.css({top:beforH + 'px', left:cardWidth * jc2 + 'px'});
+			}else if(j < cardRowLength * 4){
+				jc =	j - cardRowLength;
+				var jc3 = j - cardRowLength *3;
+				beforH = cardWrap.find('.card').eq(jc).outerHeight() +
+								 parseInt( cardWrap.find('.card').eq(jc).css('top') );
+
+				card.css({top:beforH + 'px', left:cardWidth * jc3 + 'px'});		 
+			}else{
+				card.hide();
+			}
+
 		}
 		//  -------------------------------
 	};
