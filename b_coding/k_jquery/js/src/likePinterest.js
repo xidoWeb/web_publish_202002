f288216524b86358d788ebe80ff4271c466f0be5
xido <erivery2@gmail.com>
2020-07-10 13:05:08 +0900

    scroll 처리기 내용늘어나게 만들기

--- b_coding/k_jquery/js/src/likePinterest.js
+++ b_coding/k_jquery/js/src/likePinterest.js
@@ -17,70 +17,61 @@
 	
 	var card; //  = $('.card');
 	var jc;   //  card생성시 순번체크용
-	var beforH;  // 이전라인의 동일순번째의 높이값(top값 포함)
 	// --------------------------------------
 	// 불러올 카드 높이값 설정
 	var cardHeight = [];
-	for(var i=0; i<100; i+=1){
+	for(var i=0; i<1000; i+=1){
 		cardHeight[i] = parseInt(Math.random() * 600) + 200;
 	}
 
 	// --------------------------------------
-	var CardSet = function(widthSize){
-		cardRowLength= parseInt( (widthSize - 60) / cardWidth );
-		wrap.css({width:cardRowLength * cardWidth + 'px'});
-		// console.log(cardRowLength);
-		//  -------------------------------
-		for(var j=0; j<50; j+=1){
+	var jCount;
+	var jcOffset;
+	var j=0;
+	var RepeatCard = function(c){
+		jCount = j + c || 20;
+		for(; j<jCount; j+=1){
 			cardWrap.append(cardCode);
 			card = cardWrap.find('.card').eq(j);
 			card.css({height:cardHeight[j]});
+			card.css({left:cardWidth * (j % cardRowLength) + 'px'});
 
-			if(j < cardRowLength){ 
-				card.css({top:0, left:cardWidth * j});
-
-			}else if(j < cardRowLength * 2){
-				jc =	j - cardRowLength;
-				beforH = cardWrap.find('.card').eq(jc).outerHeight();
-
-				card.css({top:beforH + 'px', left:cardWidth * jc + 'px'});
-			}else if(j < cardRowLength * 3){
-				jc =	j - cardRowLength;
-				var jc2 = j - cardRowLength *2;
-				beforH = cardWrap.find('.card').eq(jc).outerHeight() +
-								 parseInt( cardWrap.find('.card').eq(jc).css('top') );
-
-				console.log(jc, beforH);
-
-				card.css({top:beforH + 'px', left:cardWidth * jc2 + 'px'});
-			}else if(j < cardRowLength * 4){
-				jc =	j - cardRowLength;
-				var jc3 = j - cardRowLength *3;
-				beforH = cardWrap.find('.card').eq(jc).outerHeight() +
-								 parseInt( cardWrap.find('.card').eq(jc).css('top') );
-
-				card.css({top:beforH + 'px', left:cardWidth * jc3 + 'px'});		 
-			}else{
-				card.hide();
+			if(j < cardRowLength){	card.css({top:0});	}else{
+				jc = j - cardRowLength;
+				jcOffset = parseInt(cardWrap.find('.card').eq(jc).css('top')) + 
+				                    cardWrap.find('.card').eq(jc).outerHeight();
+				card.css({top:jcOffset});
 			}
+		} // for
+	}; // RepeatCard() // ------------------------------------------- end
 
-		}
-		//  -------------------------------
+	var CardSet = function(widthSize){
+		cardRowLength= parseInt( (widthSize - 60) / cardWidth );
+		wrap.css({width:cardRowLength * cardWidth + 'px'});
+		RepeatCard();
 	};
-
 	CardSet(winW);
-
 	// --------------------------------------
+
 	win.on('resize', function(){
 		var nowWinW = win.outerWidth();
-		if(winW !== nowWinW){
-			console.log('????');
-			CardSet(nowWinW);
-		}
+		if(winW !== nowWinW){	CardSet(nowWinW);	}
 	});
 	// --------------------------------------
-	
 
-	// console.log(cardHeight);
+	// $('#headBox').on('click', function(){
+	// 	RepeatCard(25);
+	// });
+
+	$(document).on('scroll', function(){
+		var nowScroll = win.scrollTop();
+		var cardOffset = card.offset().top - win.outerHeight();
+
+		if(nowScroll >= cardOffset){
+			RepeatCard(25);
+		}
+	});
+
+	
 
 })(jQuery);
